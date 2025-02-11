da496d60c0abfa7f8d684b246d1855e2965cb9d5
Saarthak15 <159225713+Saarthak15@users.noreply.github.com>
2025-02-11 09:19:21 +0530

    Create Demo.py

---
+++ Demo.py
@@ -0,0 +1,10 @@
+print("Check weather the number is odd or even")
+
+a  = int(input("Enter the number:"))
+
+if a%2  == 0:
+    print("The number is postive")
+elif a == 0:
+    print("The number is zero")
+else:
+    print("The number is negative")
